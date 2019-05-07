1f288ce51580275ab8699e73d5bcc64c9f22b431
Chen Zhou <nickey0809@gmail.com>
2019-05-07 01:05:04 -0400

    reverse pipe, sort pipe, fileter

---
+++ src/app/reverse.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reverse'
+})
+export class ReversePipe implements PipeTransform {
+
+  transform(value: string): any {
+    if(value.length ===0){
+      return value;
+    }
+    return value.split("").reverse().join("");
+
+  }
+
+}
